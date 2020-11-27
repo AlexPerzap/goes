d78093c07b13a9ded12a0fb0c007e31dba71d8db
julian <Joolean14>
2020-11-27 15:33:10 -0500

    Segunda sesion

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
-function esconderMostrar(){
-    document.querySelector(".links").classList.toggle("links-show");
+let boton = document.getElementById("hamburguesa");
+
+boton.addEventListener("click", esconderMostrar);
+
+
+function esconderMostrar() {
+    let element = document.querySelector(".links");
+    element.classList.toggle("links-show");
 }
 
-let boton = document.getElementById("hamburguesa");
-boton.addEventListener("click", esconderMostrar);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let boton = document.getElementById("hamburguesa");
+// boton.addEventListener("click", esconderMostrar);
+
+// function esconderMostrar(){
+//     let menu = document.querySelector(".links");
+    
+//     if (menu.classList.contains("links-show")){
+//         menu.classList.remove("links-show");
+//     } else{
+//     menu.classList.add("links-show");}
+// }
